5497798440dc71e0a693a1e987ae01497b6db7e7
HeeYeon-Ko <rhgmldus6@gmail.com>
2025-10-17 00:23:08 +0900

    [week4/mission]엔티티 생성

---
+++ src/main/java/com/example/umc9th/domain/restaurant/enums/RestaurantType.java
@@ -0,0 +1,12 @@
+package com.example.umc9th.domain.restaurant.enums;
+
+public enum RestaurantType {
+    KOREAN,       // 한식
+    CHINESE,      // 중식
+    JAPANESE,     // 일식
+    WESTERN,      // 양식
+    FASTFOOD,     // 패스트푸드
+    CAFE,         // 카페 / 디저트
+    BAR,          // 주점 / 펍
+    ETC           // 기타
+}
