5f114f359678478c7f85ec8a3b361deb7b2cac33
som141 <163431679+som141@users.noreply.github.com>
2025-12-02 15:10:59 +0900

    d

--- week4/demo/src/main/java/com/example/demo/domain/review/Handler/ReviewExceptionHandler.java
+++ week4/demo/src/main/java/com/example/demo/domain/review/Handler/ReviewExceptionHandler.java
@@ -3,6 +3,7 @@ package com.example.demo.domain.review.handler;
 import com.example.demo.domain.review.exception.ReviewException;
 import com.example.demo.global.apipayload.response.ApiResponse;
 import com.example.demo.domain.review.exception.code.ReviewErrorCode;
+import com.example.demo.global.apipayload.response.BaseErrorCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,7 +12,7 @@ public class ReviewExceptionHandler {
 
     @ExceptionHandler(ReviewException.class)
     public ResponseEntity<ApiResponse<Void>> handleReviewException(ReviewException e) {
-        ReviewErrorCode errorCode =e.getErrorCode();
+        BaseErrorCode errorCode =e.getErrorCode();
         return ApiResponse.ERROR(errorCode);
     }
 
